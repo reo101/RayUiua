c11838fa034e66a01ac70a958d79a58e51f1cde9
reo101 <pavel.atanasov2001@gmail.com>
2026-05-02 04:43:29 +0300

    feat(nix): export custom `uiua` and `raylib` builds as packages
    - Custom `uiua` is based on `nightly`, with all optional supports enabled

--- outputs.nix
+++ outputs.nix
@@ -53,14 +53,47 @@ inputs.flake-parts.lib.mkFlake { inherit inputs; } (
         ...
       }:
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.pkg-config
-          ];
-
-          nativeBuildInputs = [
-            pkgs.uiua
-            (pkgs.raylib.overrideAttrs (oldAttrs: rec {
+        packages = {
+          uiua =
+            (pkgs.uiua.override (
+              lib.flip lib.pipe [
+                (lib.pipe "Support" [
+                  lib.hasSuffix
+                  (
+                    f:
+                    lib.flip lib.pipe [
+                      f
+                      lib.const
+                    ]
+                  )
+                  lib.filterAttrs
+                ])
+                (lib.pipe true [
+                  lib.const
+                  lib.const
+                  lib.mapAttrs
+                ])
+              ] pkgs.uiua.override.__functionArgs
+            )).overrideAttrs
+              rec {
+                version = "unstable-2026-05-06";
+                src = pkgs.fetchFromGitHub {
+                  owner = "uiua-lang";
+                  repo = "uiua";
+                  rev = "ec5fad242ac719c126373419037a5fc0e4a69686";
+                  hash = "sha256-a7uDabQiRRO9FNWjzFZkaha29M7hNhZuAS/kIoGkGgI=";
+                };
+                cargoHash = lib.fakeHash;
+                cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                  inherit src;
+                  name = "uiua-${version}-vendor";
+                  hash = "sha256-dgT2ZRgPEBdG4gSoJJ5KV7RO7hF1dVosCd5CSUqsfHI=";
+                };
+                doCheck = false;
+                doInstallCheck = false;
+              };
+          raylib = (
+            pkgs.raylib.overrideAttrs (oldAttrs: rec {
               version = "6.0";
               src = pkgs.fetchFromGitHub {
                 owner = "raysan5";
@@ -68,10 +101,27 @@ inputs.flake-parts.lib.mkFlake { inherit inputs; } (
                 tag = version;
                 hash = "sha256-8+6MDTMc7Spix4ndAUzp51Q5iWcl7pQmyXuV2RutnOk=";
               };
-              propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-                pkgs.libxrandr
+              propagatedBuildInputs =
+                (oldAttrs.propagatedBuildInputs or [ ])
+                ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+                  pkgs.libxrandr
+                ];
+              cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+                (lib.cmakeBool "GLFW_LINUX_ENABLE_WAYLAND" true)
               ];
-            }))
+            })
+          );
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.pkg-config
+          ];
+
+          nativeBuildInputs = [
+            self'.packages.uiua
+            self'.packages.raylib
+            pkgs.sdl3
           ];
         };
       };
